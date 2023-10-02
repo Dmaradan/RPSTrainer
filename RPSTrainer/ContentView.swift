2c39b443e3fd671f5b138a9f533497ccfe94cca5
Dmaradan <diegomartin1313@gmail.com>
2023-10-02 16:31:47 +0200

    Reset game after 10 round limit with alert

--- RPSTrainer/ContentView.swift
+++ RPSTrainer/ContentView.swift
@@ -21,19 +21,28 @@ struct ContentView: View {
     @State private var roundsRemaining = 10
     @State private var resultsText = "Confirm your choice to see if you win"
     @State private var computerChoiceHidden = true
+    @State private var gameOverAlert = false
     
     var body: some View {
         VStack(spacing: 30) {
             
             Text("Your score is \(score)")
-            Text("\(roundsRemaining) rounds remaining")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             
             if !computerChoiceHidden {
-                Text(emojiStyle())
-                    .font(.system(size: 200))
+                Text(emojiStyle(for: computerChoice))
+                    .font(.system(size: 100))
             }
             
+            Spacer()
+            
             Text(playerShouldWin ? "Try to win" : "Try to lose")
+                .font(.title)
+            
+            Spacer()
+            
+            Text(emojiStyle(for: playerChoice))
+                .font(.system(size: 100))
             
             Picker("Choices: ", selection: $playerChoice) {
                 ForEach(Choice.allCases, id: \.self) {
@@ -45,17 +54,22 @@ struct ContentView: View {
             Button(computerChoiceHidden ? "Confirm Choice" : "Reset") {
                 if computerChoiceHidden {
                     computerChoiceHidden = false
-                    resultsText = checkVictory() ? "You won!" : "You lost"
+                    resultsText = checkVictory() ? "Good!" : "Nice try"
                 } else {
                     // reset game
                     setChoices()
                     roundsRemaining -= 1
+                    if roundsRemaining == 0 {
+                        gameOverAlert = true
+                    }
                 }
                 
             }
             .buttonStyle(.borderedProminent)
             
             Text(resultsText)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
             
             
         }
@@ -63,16 +77,28 @@ struct ContentView: View {
         .onAppear() {
             setChoices()
         }
+        .alert("Your final score is \(score), good effort!", isPresented: $gameOverAlert) {
+            Button("Reset") {
+                resetScore()
+                setChoices()
+            }
+        }
     }
     
     func setChoices() {
         computerChoiceHidden = true
         computerChoice = Choice.allCases[Int.random(in: 0...2)]
         playerShouldWin.toggle()
+        playerChoice = .paper
     }
     
-    func emojiStyle() -> String {
-        switch(computerChoice) {
+    func resetScore() {
+        score = 0
+        roundsRemaining = 10
+    }
+    
+    func emojiStyle(for choice: Choice) -> String {
+        switch(choice) {
         case .rock:
             "🪨"
         case .paper:
