e4be2f7cbb293913ae9ee06bb1ad8d0270eeef5e
Dmaradan <diegomartin1313@gmail.com>
2023-10-02 15:51:15 +0200

    Basic game functionality

--- RPSTrainer/ContentView.swift
+++ RPSTrainer/ContentView.swift
@@ -14,23 +14,89 @@ enum Choice: String, CaseIterable {
 }
 
 struct ContentView: View {
-    @State private var computerChoice: Choice = .rock
-    @State private var playerShouldWin: Bool = true
-    
+    @State private var computerChoice = Choice.allCases[Int.random(in: 0...2)]
+    @State private var playerShouldWin = Bool.random()
+    @State private var playerChoice = Choice.paper
+    @State private var score = 0
+    @State private var resultsText = "Confirm your choice to see if you win"
     
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Button("Print Computer Choice") {
-                selectComputerChoice()
+                setChoices()
             }
             
-            Text(computerChoice.rawValue)
+            Text(emojiStyle())
+                .font(.system(size: 200))
+            Text(playerShouldWin ? "Try to win" : "Try to lose")
+            
+            Picker("Choices: ", selection: $playerChoice) {
+                ForEach(Choice.allCases, id: \.self) {
+                    Text($0.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            Button("Confirm Choice") {
+                resultsText = checkVictory() ? "You won!" : "You lost"
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Text(resultsText)
+            
+            
         }
         .padding()
     }
     
-    func selectComputerChoice() {
+    func setChoices() {
         computerChoice = Choice.allCases[Int.random(in: 0...2)]
+        playerShouldWin.toggle()
+    }
+    
+    func emojiStyle() -> String {
+        switch(computerChoice) {
+        case .rock:
+            "🪨"
+        case .paper:
+            "📜"
+        case .scissors:
+            "✂️"
+        }
+    }
+    
+    func checkVictory() -> Bool {
+        var won = false
+        
+        // return false if tied, otherwise check who won the game, toggling if needed
+        
+        if computerChoice == .rock {
+            if playerChoice == .rock {
+                return false
+            } else if playerChoice == .paper {
+                won = true
+            } else {
+                //lose
+            }
+        } else if computerChoice == .paper {
+            if playerChoice == .paper {
+                return false
+            } else if playerChoice == .scissors {
+                won = true
+            } else {
+                //lose
+            }
+        } else {
+            if playerChoice == .scissors {
+                return false
+            } else if playerChoice == .rock {
+                won = true
+            } else {
+                //lose
+            }
+        }
+        
+        return (playerShouldWin ? won : !won)
     }
 }
 
